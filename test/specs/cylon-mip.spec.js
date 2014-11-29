1b403d300bc67069365dd3d6996c2e688267acf1
deadprogram <ron@hybridgroup.com>
2014-11-29 08:42:23 -0800

    Cleanup and add some more rudimentary tests

--- test/specs/cylon-mip.spec.js
+++ test/specs/cylon-mip.spec.js
@@ -5,9 +5,15 @@ var module = source("cylon-mip");
 var MIP = source('driver');
 
 describe("Cylon.MIP", function() {
+  describe("#drivers", function() {
+    it("is an array of provided drivers", function() {
+      expect(module.drivers).to.be.eql(["mip"])
+    });
+  });
+
   describe("#driver", function() {
-    it("returns an instance of the MIP Driver", function() {
-      var args = { device: {connection: 'test'} };
+    it("returns an instance of the MIP", function() {
+      var args = { adaptor: {} };
       expect(module.driver(args)).to.be.instanceOf(MIP);
     });
   });
