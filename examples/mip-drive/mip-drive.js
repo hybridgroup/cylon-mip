a96a4fc68fb9d8261171198e703aa0044fddf672
Andrew Stewart <andrew@stwrt.ca>
2014-12-15 14:39:58 -0800

    Organize, update examples

--- examples/mip-drive/mip-drive.js
+++ examples/mip-drive/mip-drive.js
@@ -1,14 +1,21 @@
 var Cylon = require('cylon');
 
 Cylon.robot({
-  connection: { name: 'bluetooth', adaptor: 'central', uuid: 'd03972a24e55', module: 'cylon-ble'},
-  devices: [{name: 'mip', driver: 'mip'}],
+  connections: {
+    bluetooth: { adaptor: 'central', uuid: 'd03972a24e55', module: 'cylon-ble' }
+  },
+
+  devices: {
+    mip: { driver: 'mip' }
+  },
 
   work: function(my) {
     my.mip.setHeadLED(2, 2, 2, 2);
+
     after((2).seconds(), function() {
       my.mip.distanceDrive(0, 10, 0, 0);
     });
+
     after((3).seconds(), function() {
       my.mip.setHeadLED(1, 1, 1, 1);
     });
---
+++ examples/mip/mip.js
@@ -0,0 +1,26 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    bluetooth: { adaptor: 'central', uuid: 'd03972a24e55', module: 'cylon-ble' }
+  },
+
+  devices: {
+    battery: { driver: 'ble-battery-service' },
+    deviceInfo: { driver: 'ble-device-information' },
+    mip: { driver: 'mip' }
+  },
+
+  work: function(my) {
+    my.deviceInfo.getManufacturerName(function(err, data) {
+      if (!!err) {
+        console.log("error:", err);
+        return;
+      }
+
+      console.log("data:", data);
+
+      my.mip.setHeadLED(2, 2, 2, 2);
+    });
+  }
+}).start();
